92024d2a2a1dce8724e6dce1139134c1588c54f6
nerdwerd <nerdwerd@gangoons.com>
2025-09-23 16:14:22 +0200

    Modified the overlay of godot

--- common/overlays.nix
+++ common/overlays.nix
@@ -8,21 +8,13 @@ nur,
   nixpkgs.overlays = [
     nur.overlays.default
     (final: prev: {  
-      godot = prev.godot.overrideAttrs (oldAttrs: {  
+      godot = prev.godot_4_3.overrideAttrs (oldAttrs: {  
         src = final.fetchFromGitLab {  
           owner = "polyad-misc";  
           repo = "godot";  
           rev = "4248411baf7a81b49e827f3f17d9dcc4b53b6c6a";  
           sha256 = "1xw3raajmlz7q6yxv29qichvhnjrnjsgsarfdchk06a3wdbdw5vm";  
         };  
-        buildInputs =  oldAttrs.buildInputs ++ [
-          pkgs.libdecor
-          pkgs.freetype
-          pkgs.zlib
-          pkgs.bzip2
-          pkgs.libpng
-          pkgs.harfbuzz
-        ];
       }); 
     })  
   ];
