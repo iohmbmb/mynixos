e98d23ddf49340d9814b53b53a71e9ca8ad7bbf7
nerdwerd <nerdwerd@gangoons.com>
2025-10-15 13:46:36 +0200

    Modified wallpaper

--- home/iohannes/dotfiles/.settings/wallpaper/random_wallpaper_multi.sh
+++ home/iohannes/dotfiles/.settings/wallpaper/random_wallpaper_multi.sh
@@ -18,6 +18,6 @@ fi
 for MON in "${MONITOR_ARRAY[@]}"; do
     RAND_WP="${WALLPAPERS[RANDOM % ${#WALLPAPERS[@]}]}"
     notify-send "Setting wallpaper for $MON: $RAND_WP"
-    swaybg -i "$RAND_WP" -o "$MON" 
+    swaybg -i "$RAND_WP" -m fill -o "$MON" 
 done
 
