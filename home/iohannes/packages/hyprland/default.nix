c5a821fe15ad365f742fa185038aa37f8284e604
nerdwerd <nerdwerd@gangoons.com>
2026-02-25 10:22:35 +0100

    Added new monitor for Aegis

--- home/iohannes/packages/hyprland/default.nix
+++ home/iohannes/packages/hyprland/default.nix
@@ -75,7 +75,8 @@ in
     settings = {
       monitor = (
         if (osConfig.networking.hostName == "aegis") then [
-          "HDMI-A-2, 2560x1440@60, 2560x0, 1"
+          "HDMI-A-1, 2560x1440@60, 2560x0, 1"
+          "HDMI-A-2, 2560x1440@60, auto, 1"
         ] else if (osConfig.networking.hostName == "sybils") then [
             "eDP-1,preferred,auto,auto"
             ",preferred, auto, 1, mirror, eDP-1"
@@ -115,7 +116,10 @@ in
         layout = "dwindle";
       };
 
-      workspace = ["name:1, monitor:DP-3, default:true"];
+      workspace = [
+        "name:1, monitor:HDMI-A-1, default:true"
+        "name:2, monitor:HDMI-A-2, default:true"
+      ];
 
       dwindle = {
         pseudotile = true;
