cc33cf47f94405a27ff3178c6b7c10693c77a85e
nerdwerd <nerdwerd@gangoons.com>
2026-04-06 16:36:44 +0200

    Modified binding for tmux

--- home/iohannes/packages/tmux/default.nix
+++ home/iohannes/packages/tmux/default.nix
@@ -6,17 +6,11 @@
     newSession = true;
     clock24 = true;
     extraConfig = ''
-        bind - split-window -h # Split panes horizontal
-        bind | split-window -v # Split panes vertically
-        set -g @resurrect-processes 'ssh psql mysql sqlite3'
-        set -g @resurrect-strategy-nvim 'session'
-        resurrect_dir=~/.local/share/tmux/resurrect
-        set -g @resurrect-dir $resurrect_dir
-        set -g @resurrect-hook-post-save-all "sed -i 's| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g' $(readlink -f $resurrect_dir/last)"
+        bind h split-window -h # Split panes horizontal
+        bind v split-window -v # Split panes vertically
     '';
     plugins = [
       pkgs.tmuxPlugins.vim-tmux-navigator
-      pkgs.tmuxPlugins.resurrect
     ];
   };
 }
