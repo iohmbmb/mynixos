0e2287dcbede992c921c5cd3bf92622a5864f2b9
nerdwerd <nerdwerd@gangoons.com>
2026-08-04 23:14:22 +0200

    Added hook to start ollama when starting tmux

--- home/iohannes/packages/tmux/default.nix
+++ home/iohannes/packages/tmux/default.nix
@@ -1,4 +1,11 @@
-{ pkgs, ...}:
+{ pkgs, osConfig, ...}:
+let
+  hostname = osConfig.networking.hostName;
+
+  ollamaModel = if hostname == "aegis" 
+                then "qwen2.5-coder:14b-base" 
+                else "qwen2.5-coder:7b-base";
+in
 {
   programs.tmux = {
     enable = true;
@@ -9,6 +16,8 @@
     extraConfig = ''
         bind h split-window -h # Split panes horizontal
         bind v split-window -v # Split panes vertically
+        # Create a background session named 'qwen' running your model with a fake screen size
+      new-session -d -s qwen -x 80 -y 24 'ollama run ${ollamaModel}'
     '';
     plugins = [
       pkgs.tmuxPlugins.vim-tmux-navigator
