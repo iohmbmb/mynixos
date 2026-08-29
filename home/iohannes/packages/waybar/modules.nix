042ff868165a8f331bd7102d1dfd0d216458f4f5
nerdwerd <nerdwerd@gangoons.com>
2026-08-30 01:18:04 +0200

    added wiremix and impala to the on-click method of audio and network modules

--- home/iohannes/packages/waybar/modules.nix
+++ home/iohannes/packages/waybar/modules.nix
@@ -44,7 +44,7 @@
       car = "";
       default = [ "" "" "" ];
     };
-    on-click = "pavucontrol"; # Opens your volume mixer when clicked
+    on-click = "kitty wiremix"; # Opens your volume mixer when clicked
   };
 
   # Added the system tray configuration block
@@ -61,6 +61,7 @@
     tooltip-format = "IP: {ipaddr}\ Gateway: {gwaddr}\ Interface: {ifname}";
     tooltip-format-wifi = "SSID: {essid}\ IP: {ipaddr}\ Gateway: {gwaddr}\ Interface: {ifname}";
     tooltip-format-ethernet = "Ethernet\ IP: {ipaddr}\ Gateway: {gwaddr}\ Interface: {ifname}";
+    on-click = "kitty impala";
   };
 
   "battery" = {
