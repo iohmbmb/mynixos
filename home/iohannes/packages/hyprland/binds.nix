680dd0fb46c1f6fbc7ab0a0bd327d2b918d3130c
nerdwerd <nerdwerd@gangoons.com>
2026-09-07 00:30:38 +0200

    changed the binding of marvin to thunderbird

--- home/iohannes/packages/hyprland/binds.nix
+++ home/iohannes/packages/hyprland/binds.nix
@@ -17,7 +17,7 @@
       "$mod, U, exec, unityhub"
       "$mod, G, exec, godot"
       "$mod, O, exec, [float] obsidian"
-      "$mod, M, exec, [workspace 1] marvin"
+      "$mod, M, exec, thunderbird"
       "$mod, S, exec, kitty wiremix"
       "$mod, B, exec, [workspace 3] blender"
       "$mod_SHIFT, B, exec, $term bluetuith"
